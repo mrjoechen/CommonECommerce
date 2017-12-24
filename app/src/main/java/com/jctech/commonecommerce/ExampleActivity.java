5d4fb50728d8fa77378235ed63417342524f19bd
mrjoechen <mrjoechen@qq.com>
2017-12-24 23:44:30 +0800

    登录完成

--- app/src/main/java/com/jctech/commonecommerce/ExampleActivity.java
+++ app/src/main/java/com/jctech/commonecommerce/ExampleActivity.java
@@ -46,6 +46,7 @@ public class ExampleActivity extends ProxyActivity implements
         switch (tag) {
             case SIGNED:
                 Toast.makeText(this, "启动结束，用户登录了", Toast.LENGTH_LONG).show();
+                startWithPop(new ExampleDelegate());
                 break;
             case NOT_SIGNED:
                 Toast.makeText(this, "启动结束，用户没登录", Toast.LENGTH_LONG).show();
