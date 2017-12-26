56f4d6b90b12e109a839b66895585a91f50ca640
mrjoechen <mrjoechen@qq.com>
2017-12-26 23:44:46 +0800

    annotation compiler

---
+++ lib-core-android/src/main/java/com/jctech/lib_core_android/wx/templete/WXPayEntryTemplete.java
@@ -0,0 +1,15 @@
+package com.jctech.lib_core_android.wx.templete;
+
+import com.jctech.lib_core_android.activities.ProxyActivity;
+import com.jctech.lib_core_android.delegate.ECDelategate;
+
+/**
+ * Created by CHENQIAO on 2017/12/26.
+ */
+
+public class WXPayEntryTemplete extends ProxyActivity {
+    @Override
+    public ECDelategate setRootDelegate() {
+        return null;
+    }
+}
