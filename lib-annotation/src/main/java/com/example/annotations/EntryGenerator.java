59d983649a60f243515306da268a335294daf6af
mrjoechen <mrjoechen@qq.com>
2017-12-26 23:23:55 +0800

    annotation compiler

---
+++ lib-annotation/src/main/java/com/example/annotations/EntryGenerator.java
@@ -0,0 +1,20 @@
+package com.example.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by CHENQIAO on 2017/12/26.
+ */
+
+
+@Target(ElementType.TYPE)//声明注解使用在类上面
+@Retention(RetentionPolicy.SOURCE)//编译器在编译源码阶段处理
+public @interface EntryGenerator {
+
+    String packageName();
+
+    Class<?> entryTemplete();
+}
