e7b92e3f8022e222921a869a32f64be5384457ed
mrjoechen <mrjoechen@qq.com>
2018-01-04 23:14:37 +0800

    import wechat sdk

--- lib-core-android/src/main/java/com/jctech/lib_core_android/wx/BaseWXActivity.java
+++ lib-core-android/src/main/java/com/jctech/lib_core_android/wx/BaseWXActivity.java
@@ -4,9 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
-import com.tencent.mm.opensdk.modelbase.BaseReq;
-import com.tencent.mm.opensdk.modelbase.BaseResp;
 import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
 
 /**
@@ -21,6 +18,7 @@ public abstract class BaseWXActivity extends AppCompatActivity implements IWXAPI
         //这个必须写在onCreate中
         ECWeChat.getInstance().getWXAPI().handleIntent(getIntent(), this);
     }
+
 
     @Override
     protected void onNewIntent(Intent intent) {
