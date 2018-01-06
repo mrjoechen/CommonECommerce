6170a8834303586ac09b99c799d0f135d1a6cfbf
mrjoechen <mrjoechen@qq.com>
2018-01-06 23:38:35 +0800

    底部导航

--- lib-core-android/src/main/java/com/jctech/lib_core_android/delegate/bottom/ItemBuilder.java
+++ lib-core-android/src/main/java/com/jctech/lib_core_android/delegate/bottom/ItemBuilder.java
@@ -1,10 +1,28 @@
 package com.jctech.lib_core_android.delegate.bottom;
 
+import java.util.LinkedHashMap;
+
 /**
  * @author Created by CHENQIAO on 2018/1/6.
  */
 
 public final class ItemBuilder {
 
+    private final LinkedHashMap<BottomTabBean, BottomItemDelegate> ITEMS = new LinkedHashMap<>();
 
+    static ItemBuilder builder(){
+        return new ItemBuilder();
+    }
+
+    public final ItemBuilder addItem(BottomTabBean bottomTabBean, BottomItemDelegate bottomItemDelegate){
+
+        ITEMS.put(bottomTabBean, bottomItemDelegate);
+        return this;
+    }
+
+    public final ItemBuilder addItem(LinkedHashMap<BottomTabBean, BottomItemDelegate> bottomTabBean){
+
+        ITEMS.putAll(bottomTabBean);
+        return this;
+    }
 }
