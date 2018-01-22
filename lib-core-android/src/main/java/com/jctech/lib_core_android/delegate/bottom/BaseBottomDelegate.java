c9d7a464754d7582740aed5637264b7e6ca956de
mrjoechen <mrjoechen@qq.com>
2018-01-22 22:19:32 +0800

    底部导航

--- lib-core-android/src/main/java/com/jctech/lib_core_android/delegate/bottom/BaseBottomDelegate.java
+++ lib-core-android/src/main/java/com/jctech/lib_core_android/delegate/bottom/BaseBottomDelegate.java
@@ -4,18 +4,29 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import com.jctech.lib_core_android.R;
+import com.jctech.lib_core_android.R2;
 import com.jctech.lib_core_android.delegate.ECDelategate;
+import com.joanzapata.iconify.widget.IconTextView;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import butterknife.BindView;
+import me.yokeyword.fragmentation.ISupportFragment;
+
 /**
  * Created by CHENQIAO on 2018/1/6.
  */
 
-public abstract class BaseBottomDelegate extends ECDelategate  {
+public abstract class BaseBottomDelegate extends ECDelategate implements View.OnClickListener{
 
 
     private final ArrayList<BottomItemDelegate> ITEM_DELEGATES = new ArrayList<>();
@@ -24,6 +35,9 @@ public abstract class BaseBottomDelegate extends ECDelategate  {
     private int mCurrentDelegate = 0;
     private int mIndexDelegate = 0;
     private int mClickedColor = Color.RED;
+
+    @BindView(R2.id.bottom_bar)
+    LinearLayoutCompat mBottomBar = null;
 
     public abstract LinkedHashMap<BottomTabBean, BottomItemDelegate> setItems(ItemBuilder itemBuilder);
 
@@ -50,5 +64,39 @@ public abstract class BaseBottomDelegate extends ECDelategate  {
             TAB_BEANS.add(key);
             ITEM_DELEGATES.add(value);
         }
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+
+        final int size = ITEMS.size();
+
+        for (int i = 0; i< size; i++){
+            LayoutInflater.from(getContext()).inflate(R.layout.bottom_item_icon_text_layout, mBottomBar);
+            final RelativeLayout item = (RelativeLayout) mBottomBar.getChildAt(i);
+            //设置每个item的点击事件
+            item.setTag(i);
+            item.setOnClickListener(this);
+            final IconTextView itemIcon = (IconTextView) item.getChildAt(0);
+            final AppCompatTextView itemTitle = (AppCompatTextView) item.getChildAt(1);
+            final BottomTabBean bean = TAB_BEANS.get(i);
+            //初始化数据
+            itemIcon.setText(bean.getICON());
+            itemTitle.setText(bean.getNAME());
+            if (i == mIndexDelegate) {
+                itemIcon.setTextColor(mClickedColor);
+                itemTitle.setTextColor(mClickedColor);
+            }
+        }
+
+        final ISupportFragment[] delegateArray = ITEM_DELEGATES.toArray(new ISupportFragment[size]);
+        getSupportDelegate().loadMultipleRootFragment(R.id.bottom_bar_delegate_container, mIndexDelegate, delegateArray);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
