ccd2aba4f22fcc4eff9df02879ec9f74489ec5a8
mrjoechen <mrjoechen@qq.com>
2018-01-26 23:54:15 +0800

    底部导航

---
+++ lib-ec-android/src/main/java/com/jctech/lib_ec_android/main/EcBottomDelegate.java
@@ -0,0 +1,34 @@
+package com.jctech.lib_ec_android.main;
+
+import com.jctech.lib_core_android.delegate.bottom.BaseBottomDelegate;
+import com.jctech.lib_core_android.delegate.bottom.BottomItemDelegate;
+import com.jctech.lib_core_android.delegate.bottom.BottomTabBean;
+import com.jctech.lib_core_android.delegate.bottom.ItemBuilder;
+
+import java.util.LinkedHashMap;
+
+/**
+ * Created by CHENQIAO on 2018/1/26.
+ */
+
+public class EcBottomDelegate extends BaseBottomDelegate{
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public LinkedHashMap<BottomTabBean, BottomItemDelegate> setItems(ItemBuilder itemBuilder) {
+        return null;
+    }
+
+    @Override
+    public int setIndexDelegate() {
+        return 0;
+    }
+
+    @Override
+    public int setClickedColor() {
+        return 0;
+    }
+}
